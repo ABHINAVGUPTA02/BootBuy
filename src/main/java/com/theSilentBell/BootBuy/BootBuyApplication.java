17f68ac7c0718e715512a59c86a9cb3d5a83738a
ABHINAVGUPTA02 <abhinav.gupta.dev05@gmail.com>
2025-09-12 13:27:35 +0530

    initial commit

---
+++ src/main/java/com/theSilentBell/BootBuy/BootBuyApplication.java
@@ -0,0 +1,13 @@
+package com.theSilentBell.BootBuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theSilentBell/BootBuy/BootBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theSilentBell.BootBuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
